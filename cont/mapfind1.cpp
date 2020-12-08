cd76dfbda1ef5b38b5d0853ab7eb3732bf429722
kpliu <kpliu@qq.com>
2020-12-08 08:47:23 +0800

    map

---
+++ cont/mapfind1.cpp
@@ -0,0 +1,25 @@
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    map<float,float> coll = { {1,7}, {2,4}, {3,2}, {4,3},
+                            {5,6}, {6,1}, {7,3}, {3,8}, {9,3} };
+
+    for(auto elem : coll){
+        cout << elem.first << ":" << elem.second << endl;
+    }
+    auto posKey = coll.find(3.0);
+    if(posKey != coll.end()){
+        cout << "key 3.0 found (" << posKey->first << ": " << posKey->second << ")" << endl;
+    }
+
+    auto posVal = find_if(coll.begin(), coll.end(), [](const pair<float, float>& elem){return elem.second == 3.0;});
+    if(posVal != coll.end()){
+        cout << "val 3.0 found (" << posVal->first << ": " << posVal->second << ")" << endl;
+    }
+}
---
+++ cont/newkey.hpp
@@ -0,0 +1,15 @@
+namespace MyLib{
+    template<typename Cont>
+    inline
+    bool replace_key(Cont& c, const typename Cont::key_type& old_key, const typename Cont::key_type& new_key){
+        typename Cont::iterator pos;
+        pos = c.find(old_key);
+        if(pos != cend()) {
+            c.insert(typename Cont::value_type(new_key, pos->second));
+            c.erase(pos);
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
