9abcc4ed03dc35157046fdd120796619dff98d37
kpliu <kpliu.qq.com>
2020-12-19 15:08:11 +0800

    cstylearray

---
+++ cont/cstylearray1old.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    int coll[] = {5,6,2,4,1,3};
+
+    transform(coll, coll+6, coll, coll, multiplies<int>());
+
+    sort(coll+1, coll+6);
+
+    copy(coll, coll+6, ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
